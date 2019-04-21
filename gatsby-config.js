09be8c1a5b4590b75a6115d841f5c3ebc427d449
Divyanshu Maithani <div.blackcat@gmail.com>
2019-04-21 22:47:04 +0530

    Some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `SagarK's Blog`,
+    author: `Sagar Karira`,
+    description: `Personal blog of Sagar Karira`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ekrysis`,
     },
   },
   plugins: [
--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Build with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
