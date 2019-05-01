c41a4524f2e76ffb2eb87d6b917e5aefc1400230
Divyanshu Maithani <div.blackcat@gmail.com>
2019-05-01 17:26:38 +0530

    Fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,20 +3,34 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useDarkMode } from "../utils/hooks"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const [darkMode, toggleDarkMode] = useDarkMode()
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
+  return (
+    <div
+      style={{
+        color: "#424242",
+        backgroundColor: darkMode ? "#212121" : "white",
+        height: "100%",
+        minHeight: "100vh",
+        transition: "0.4s ease-in-out",
+      }}
+    >
+      <Layout
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+        location={location}
+        title={siteTitle}
+      >
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
-    )
-  }
+    </div>
+  )
 }
 
 export default NotFoundPage
