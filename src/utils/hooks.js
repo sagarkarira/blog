9ad9aa54c6fe11abc35d5695cd51e116d01a2829
sagark <sagarkarira1992@gmail.com>
2019-05-01 23:36:48 +0530

    fixed localStorage issue

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,11 +1,15 @@
 import { useState } from "react"
 
 export const useDarkMode = () => {
-  const initialState = JSON.parse(localStorage.getItem("darkMode")) || false
-  const [darkMode, setDarkMode] = useState(initialState)
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-    localStorage.setItem("darkMode", !darkMode)
-  }
-  return [darkMode, toggleDarkMode]
+	if (typeof window !== "undefined") {
+		const initialState = JSON.parse(localStorage.getItem("darkMode")) || false
+		const [darkMode, setDarkMode] = useState(initialState)
+		const toggleDarkMode = () => {
+		  setDarkMode(!darkMode)
+		  localStorage.setItem("darkMode", !darkMode)
+		}
+		return [darkMode, toggleDarkMode]		
+	}
+	// builds on node env
+	return [false, () => {}]
 }
